5c21ffc379788c5761edbf68e7a5c4a0e9fdf888
DamiAdeeso <damilolaadeeso@gmail.com>
2023-11-13 01:20:02 +0100

    integrated password mail

--- src/main/java/com/example/springbootbackend/controller/UserController.java
+++ src/main/java/com/example/springbootbackend/controller/UserController.java
@@ -2,7 +2,7 @@ package com.example.springbootbackend.controller;
 
 import com.example.springbootbackend.dto.LoginDto;
 import com.example.springbootbackend.dto.UserDto;
-import com.example.springbootbackend.service.UserService;
+import com.example.springbootbackend.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,20 +13,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/accounts")
 public class UserController {
 
-    private UserService userService;
+    private UserServiceImpl userServiceImpl;
 
     @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserController(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> loginUser(@RequestBody LoginDto loginDto) throws UsernameNotFoundException {
-        return userService.login(loginDto);
+        return userServiceImpl.login(loginDto);
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> saveUser(@RequestBody UserDto userDto){
-        return userService.saveUser(userDto);
+
+        System.out.println(userDto);
+        return userServiceImpl.saveUser(userDto);
     }
 }
---
+++ src/main/java/com/example/springbootbackend/mail/EmailService.java
@@ -0,0 +1,32 @@
+package com.example.springbootbackend.mail;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendPasswordMail(String to,String password) {
+        String subject = "Welcome to Egora";
+        String body = "This is your secure generated password:" + password;
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        javaMailSender.send(message);
+    }
+}
